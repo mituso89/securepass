5c53aa6e2464626f711c1663ccda3fbcbb5eeb79
tuan nguyen <tuan@diversely.io>
2025-04-27 06:37:37 +0700

    Initial commit: SecurePassLib v1.0.0

--- setup.py
+++ setup.py
@@ -2,10 +2,34 @@ from setuptools import setup, find_packages
 
 setup(
     name='securepasslib',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
-    description='Password validation and generation library',
+    description='Secure password validation, strength analysis, generation, and breach checking library',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     author='Tuan Nguyen',
     author_email='nguyenhuutuan1306@gmail.com',
-    install_requires=[],
+    url='https://github.com/yourusername/securepasslib',  # replace with your GitHub repo
+    project_urls={
+        'Documentation': 'https://github.com/yourusername/securepasslib',
+        'Source': 'https://github.com/yourusername/securepasslib',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'securepass=securepasslib.cli:cli',
+        ],
+    },
 )
